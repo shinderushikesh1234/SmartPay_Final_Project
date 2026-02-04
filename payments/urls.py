423d115d403c26c153d74dbe6c773483679f7e1b
Rushikesh tanaji shinde <shinderushikesh7108@gmail.com>
2026-02-04 19:38:09 +0530

    .

--- payments/urls.py
+++ payments/urls.py
@@ -1,3 +1,10 @@
 from django.urls import path
-from .views import home, register, pay, success, history
-urlpatterns = [path('',home,name='home'),path('register/',register,name='register'),path('pay/<int:merchant_id>/',pay,name='pay'),path('success/',success,name='success'),path('history/',history,name='history')]
+from . import views
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('pay/<int:merchant_id>/', views.pay, name='pay'),
+    path('success/', views.success, name='success'),
+    path('history/', views.history, name='history'),
+    path('register/', views.register, name='register'),
+]
